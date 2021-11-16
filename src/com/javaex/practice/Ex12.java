55a384046c8553e01c95518c16e676c818e4ea6e
Hojun_Yoo <lisiee0@naver.com>
2021-11-16 17:12:09 +0900

    modify

--- src/com/javaex/practice/Ex12.java
+++ src/com/javaex/practice/Ex12.java
@@ -17,30 +17,34 @@ public class Ex12 {
 		System.out.print("숫자2: ");
 		int num2= sc.nextInt();
 		
-		double resultA= num1+num2;
-		double resultS= num1-num2;
-		double resultM= num1*num2;
-		double resultD= num1/num2;
+		double result;
+
 		
 		switch(oper) {
 			case "+":
-				System.out.println("결과는 "+resultA);
+				result= num1+num2;
+				System.out.println("결과는 "+result);
 				break;
 				
 			case "-":
-				System.out.println("결과는 "+resultS);
+				result= num1-num2;
+				System.out.println("결과는 "+result);
 				break;
 				
 			case "*":
-				System.out.println("결과는 "+resultM);
+				result= num1*num2;
+				System.out.println("결과는 "+result);
 				break;
 				
 			case "/":
+				
 				if(num2==0) {
 					System.out.println("계산할 수 없습니다.");
 					break;
 				}
-				System.out.println("결과는 "+resultD);
+			
+				result= num1/num2;
+				System.out.println("결과는 "+result);
 				break;
 				
 			default :
